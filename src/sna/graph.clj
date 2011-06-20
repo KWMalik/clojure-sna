6eda37d80b051b2a8762f1fafbdf467711215212
Christopher Roach <vthakr@gmail.com>
2011-06-19 17:16:06 -0700

    Added copyright statements

--- src/sna/graph.clj
+++ src/sna/graph.clj
@@ -1,8 +1,8 @@
-;; Copyright (c) Christopher Roach. All rights reserved.
+;; Copyright (c) Christopher Roach. All rights reserved.  
 ;; The use and distribution terms for this software are covered by the
-;; Eclipse Public License 1.0 which can be found in the file LICENSE at
-;; the root of this distribution. By using this software in any fashion,
-;; you are agreeing to be bound by the terms of this license.
+;; Eclipse Public License 1.0 which can be found in the file LICENSE
+;; at the root of this distribution. By using this software in any
+;; fashion, you are agreeing to be bound by the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
 (ns sna.graph)
